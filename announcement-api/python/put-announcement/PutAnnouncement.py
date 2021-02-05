8e2acab531ff133dfb87f6eab6796f2fbaddd4af
Sidharth Ramesh <partner.sidharth.ramesh@philips.com>
2021-02-05 11:00:44 +0530

    - removed input validation in lambda function (moved basic request validation to api gateway)

--- announcement-api/python/put-announcement/PutAnnouncement.py
+++ announcement-api/python/put-announcement/PutAnnouncement.py
@@ -11,21 +11,15 @@ DATE_ATTRIBUTE = "date"
 dynamo = boto3.resource('dynamodb')
 announcementTable = dynamo.Table(ANNOUNCEMENT_TABLE_NAME)
 
-def validate_input(event):
-    return TITLE_ATTRIBUTE in event and DATE_ATTRIBUTE in event and event[TITLE_ATTRIBUTE] and event[DATE_ATTRIBUTE]
-
 def lambda_handler(event, context):
     print("Handling event ", event)
     
-    if validate_input(event):
-        new_item = {
-            "AnnouncementId": str(uuid.uuid4()),
-            "title": event[TITLE_ATTRIBUTE],
-            "date": event[DATE_ATTRIBUTE]
-        }
+    new_item = {
+        "AnnouncementId": str(uuid.uuid4()),
+        "title": event[TITLE_ATTRIBUTE],
+        "date": event[DATE_ATTRIBUTE]
+    }
         
-        announcementTable.put_item(Item=new_item)
-    else:
-        print("Invalid input received.")
-
+    announcementTable.put_item(Item=new_item)
+    
     return {'status':200, 'message':'success'}
