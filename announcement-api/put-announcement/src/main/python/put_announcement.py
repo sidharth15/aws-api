8165a38dee2750eb8abe5738ded940c660031901
Sidharth Ramesh <partner.sidharth.ramesh@philips.com>
2021-02-24 11:49:00 +0530

    - updated OpenAPI specification. - added error response handling for put_announcement api

--- announcement-api/put-announcement/src/main/python/put_announcement.py
+++ announcement-api/put-announcement/src/main/python/put_announcement.py
@@ -1,19 +1,20 @@
 import json
 import uuid
 import boto3
-
-print("Initializing lambda")
+import logging
 
 ANNOUNCEMENT_TABLE_NAME = "announcements"
 TITLE_ATTRIBUTE = "title"
 DATE_ATTRIBUTE = "date"
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.debug("Initializing lambda")
+
 dynamo = boto3.resource('dynamodb')
 announcementTable = dynamo.Table(ANNOUNCEMENT_TABLE_NAME)
 
-def lambda_handler(event, context):
-    print("Handling event ", event)
-    
+def handle_event(event):
     new_item = {
         "AnnouncementId": str(uuid.uuid4()),
         "title": event[TITLE_ATTRIBUTE],
@@ -21,5 +22,14 @@ def lambda_handler(event, context):
     }
         
     announcementTable.put_item(Item=new_item)
+
+def lambda_handler(event, context):
+    logger.debug("Handling event " + str(event))
+    
+    try:
+        handle_event(event)
+    except Exception as e:
+        logger.error("Error occurred while handling event: " + repr(e))
+        raise Exception("Internal Server Error")
     
     return {'status':200, 'message':'success'}
